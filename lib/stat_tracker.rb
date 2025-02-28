65ebffe5de2cad7be2e3f4c46afa7cf7c68bb0d1
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-28 15:45:03 -0800

    work together push

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,6 @@
 require 'csv'  # Requires the CSV library to handle CSV file operations
 
-class Stat_Tracker
+class StatTracker
   attr_reader :games, :teams, :game_teams  # Defines getter methods for games, teams, and game_teams
 
   # Initializes a new instance of StatTracker with games, teams, and game_teams data
@@ -21,5 +21,92 @@ class Stat_Tracker
     
     # Creates and returns a new instance of StatTracker with the read data
     self.new(games, teams, game_teams)
+  end
+
+  # Game Statistics
+  def highest_total_score
+    
+  end
+
+  def lowest_total_score
+    
+  end
+
+  def percentage_home_wins
+    
+  end
+
+  def percentage_visitor_wins
+    
+  end
+
+  def percentage_ties
+     
+  end
+
+  def count_of_games_by_season
+    
+  end
+
+  def average_goals_per_game
+    
+  end
+
+  def average_goals_by_season
+    
+  end
+
+  # League Statistics
+  def count_of_teams
+    
+  end
+
+  def best_offense
+    
+  end
+
+  def worst_offense
+    
+  end
+
+  def highest_scoring_visitor
+     
+  end
+
+  def highest_scoring_home_team
+    
+  end
+
+  def lowest_scoring_visitor
+    
+  end
+
+  def lowest_scoring_home_team
+    
+  end
+
+  # Season Statistics
+  def winningest_coach(season_id)
+    
+  end
+
+  def worst_coach(season_id)
+    
+  end
+
+  def most_accurate_team(season_id)
+    
+  end
+
+  def least_accurate_team(season_id)
+    
+  end
+
+  def most_tackles(season_id)
+    
+  end
+
+  def fewest_tackles(season_id)
+    
   end
 end
