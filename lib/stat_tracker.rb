60f2da2fbe62e69d4ae79ad5fc1fb8d0d295f7bc
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-28 15:27:36 -0800

    file_io and test push

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,25 @@
+require 'csv'  # Requires the CSV library to handle CSV file operations
+
+class Stat_Tracker
+  attr_reader :games, :teams, :game_teams  # Defines getter methods for games, teams, and game_teams
+
+  # Initializes a new instance of StatTracker with games, teams, and game_teams data
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
+
+  # Class method to create a new instance of StatTracker from CSV files
+  def self.from_csv(locations)
+    # Reads the games CSV file and converts headers to symbols
+    games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    # Reads the teams CSV file and converts headers to symbols
+    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    # Reads the game_teams CSV file and converts headers to symbols
+    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+    
+    # Creates and returns a new instance of StatTracker with the read data
+    self.new(games, teams, game_teams)
+  end
+end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'  # Requires the spec_helper file for configuration and setup
+require 'spec_helper'
 
 # Configures RSpec to use the documentation formatter
 RSpec.configure do |config|
@@ -6,28 +6,44 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Stat_Tracker do
-  # Runs before each test to set up the necessary objects
-  before(:each) do
-    
+  before(:each) do # we are just going to use the default CSV files that we have here from now (copy pasted from runner.rb)
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+
+    @stat_tracker = Stat_Tracker.from_csv(@locations)
   end
 
   describe 'initialization' do
     it '#initialize' do
-      
+      expect(@stat_tracker).to be_an_instance_of(Stat_Tracker)
     end
 
     it '#attributes' do
-      
+      expect(@stat_tracker.games).not_to be_empty
+      expect(@stat_tracker.teams).not_to be_empty
+      expect(@stat_tracker.game_teams).not_to be_empty
     end
   end
 
   # Placeholder for instance methods tests
   describe 'instance methods' do
-    
+    # Add tests for instance methods here
   end
 
-  # Placeholder for class methods tests
+  # class methods only (keyword 'self')
   describe 'class methods' do
-    
+    it '::from_csv' do
+      expect(@stat_tracker).to be_an_instance_of(Stat_Tracker)
+      expect(@stat_tracker.games).not_to be_empty
+      expect(@stat_tracker.teams).not_to be_empty
+      expect(@stat_tracker.game_teams).not_to be_empty
+    end
   end
 end
