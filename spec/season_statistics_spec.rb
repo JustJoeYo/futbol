c74622979e5381f8d899bee41af2a8c142bf3a34
Katya Weicht <katya.weicht@gmail.com>
2025-02-28 21:11:52 -0700

    Create fixture csv files

--- spec/season_statistics_spec.rb
+++ spec/season_statistics_spec.rb
@@ -7,10 +7,12 @@ RSpec.configure do |config|
 RSpec.describe SeasonStatistics do
     before(:each) do # we are just going to use the default CSV files that we have here from now (copy pasted from runner.rb)
 
-        @game_teams_path = CSV.read('./data/game_teams.csv', headers: true, header_converters: :symbol)
+        @game_teams_path = CSV.read('./data/game_team_fixture.csv', headers: true, header_converters: :symbol)
         @games_path = CSV.read('./data/games.csv', headers: true, header_converters: :symbol)
 
         @season_statistics = SeasonStatistics.new(@game_teams_path,@games_path)
+
+        #binding.pry
         
       end
     describe 'initialization' do
@@ -24,7 +26,7 @@ RSpec.describe SeasonStatistics do
     end
     describe 'instance methods' do
         it 'has a winningest_coach' do
-
+            #expect(@season_statistics.winningest_coach())
         end
 
         it 'has a worst coach' do
