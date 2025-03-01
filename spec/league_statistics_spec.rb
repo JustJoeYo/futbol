543eaab59eab52bce47119ef1f91dbe6a8ed825b
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-28 22:27:32 -0500

    adds in fixtures

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -29,16 +29,14 @@ RSpec.describe LeagueStatistics do
     describe '#best_offense' do #May want to stub
         it 'returns the team with the highest average goals scored per game' do
 
-            allow(@league_statistics).to receive(:best_offense).and_return("FC Cincinnati")
-            expect(@league_statistics.best_offense).to_eq("FC Cincinnati")
+            expect(@league_statistics.best_offense).to eq('FC Dallas')
         end
     end
 
     describe '#worst_offense' do
         it 'returns the team with the lowest average goals scored per game' do
 
-            allow(@league_statistics).to receive(:worst_offense).and_return("Chicago Fire")
-            expect(@league_statistics.worst_offense).to_eq("Chicago Fire")
+            expect(@league_statistics.worst_offense).to_eq("Sporting Kansas City")
         end
     end
 
