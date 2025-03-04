c5f4268aaea861114800a26eb957b8a74f33a7e0
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-04 17:13:18 -0500

    Refactors to pass spec harness tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -29,6 +29,62 @@ class StatTracker
     self.new(games, teams, game_teams)
   end
 
+  #Game Statistics
+  def highest_total_score
+    @game_statistics.highest_total_score
+  end
+
+  def lowest_total_score
+    @game_statistics.lowest_total_score
+  end
+
+  def percentage_home_wins
+    @game_statistics.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    @game_statistics.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @game_statistics.percentage_ties
+  end
+
+  def count_of_games_by_season
+    @game_statistics.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    @game_statistics.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    @game_statistics.average_goals_by_season
+  end
+
+  #League Statistics
+  def count_of_teams
+    @league_statistics.count_of_teams
+  end
+  def best_offense
+    @league_statistics.best_offense
+  end
+  def worst_offense
+    @league_statistics.worst_offense
+  end
+  def highest_scoring_visitor
+    @league_statistics.highest_scoring_visitor
+  end
+  def highest_scoring_home_team
+    @league_statistics.highest_scoring_home_team
+  end
+  def lowest_scoring_visitor
+    @league_statistics.lowest_scoring_visitor
+  end
+  def lowest_scoring_home_team
+    @league_statistics.lowest_scoring_home_team
+  end
+
   #Season Statistics
   def winningest_coach(season_id)
     @season_statistics.winningest_coach(season_id)
@@ -49,6 +105,4 @@ class StatTracker
     @season_statistics.fewest_tackles(season_id)
   end
 
-  #League Statistics
-  
 end
