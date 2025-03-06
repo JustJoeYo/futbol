a99b4ba1ee855d63186688b7a8e83c3ab7cf8d2f
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-05 16:51:36 -0800

    initial

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,7 @@ require './lib/game_statistics.rb'
 require './lib/season_statistics.rb'
 require './lib/league_statistics.rb'
 require './lib/team_statistics.rb'
+
+require './lib/game.rb'
+require './lib/team.rb'
+require './lib/game_team.rb'
