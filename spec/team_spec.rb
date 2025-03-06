5042b7bd26662b199b89c69d4ec8617ce762b6f9
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-06 15:16:52 -0500

    Finishes teams tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Team do
     before(:each) do
         @team_data = {
             team_id: "1",
-            franchiseId: "23",
-            teamName: "Atlanta United",
+            franchise_id: "23",
+            team_name: "Atlanta United",
             abbreviation: "ATL",
-            Stadium: "Mercedes-Benz Stadium",
+            stadium: "Mercedes-Benz Stadium",
             link: "/api/v1/teams/1"
         }
         @team = Team.new(@team_data)
@@ -24,9 +24,10 @@ RSpec.describe Team do
 
         it 'initializes with correct attributes' do
             expect(@team.team_id).to eq("1")
-            expect(@team.franchiseId).to eq("23")
-            expect(@team.teamName).to eq("Atlanta United")
+            expect(@team.franchise_id).to eq("23")
+            expect(@team.teamname).to eq("Atlanta United")
             expect(@team.abbreviation).to eq("ATL")
-            expect(@team.Stadium)
+            expect(@team.stadium).to eq("Mercedes-Benz Stadium")
+            expect(@team.link).to eq("/api/v1/teams/1")
         end
     end
