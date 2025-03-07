4c0ffe59c11edac15f81bc4a8f040b652a3d5ef1
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-07 11:37:12 -0500

    Adds in tests for biggest_team_blowout, worst_loss, & seasonal_summary

--- spec/team_statistics_spec.rb
+++ spec/team_statistics_spec.rb
@@ -61,22 +61,36 @@ RSpec.describe TeamStatistics do
 
     describe '#rival' do
       it 'returns the opponent with the highest win percentage against the given team' do
-        expect(@team_statistics.rival('6')).to eq("Houston Dynamo")
+        expect(@team_statistics.rival('6')).to eq("Sporting Kansas City")
         expect(@team_statistics.rival('3')).to eq("FC Dallas")
       end
 
       it 'returns 'No Rival Found" if the team has no opponents" do
-        expect(@team_statistics.rival('3')).to eq("FC Dallas")
+        expect(@team_statistics.rival('3')).to eq("No Rival Found")
       end
       
     end
 
-    xit '#biggest_team_blowout' do
-        
+    describe '#biggest_team_blowout' do
+      it 'returns the largest margin of victory for a given team' do
+        expect(@team_statistics.biggest_team_blowout('6')).to eq(3) #FC Dallas beat Sporting KC 4-1
+        expect(@team_statistics.biggest_team_blowout('3')).to eq(1) #Houston Dynamo biggest win was 2-1
+      end
+
+      it 'returns 0 if the team has no wins' do
+        expect(@team_statistics.biggest_team_blowout('9999')).to eq(0) #Fake
+      end
     end
 
-    xit '#worst_loss' do
-        
+    describe '#worst_loss' do
+      it 'returns the biggest loss for a given team' do
+        expect(@team_statistics.worst_loss('6')).to eq(1) #FC Dallas lost 1-3 to Houston
+        expect(@team_statistics.worst_loss('3')).to eq(2) #Houston Dynamo lost 1-3 to FC
+      end
+
+      it 'returns 0 if the team has no losses' do
+        expect(@team_statistics.worst_loss('9999')).to eq(0) #Fake
+      end
     end
 
     xit '#head_to_head' do
@@ -84,6 +98,28 @@ RSpec.describe TeamStatistics do
     end
 
     xit '#seasonal_summary' do
+      it 'returns a hash with season statistics for a given team' do
+        expected_summary = {
+          "20122013" => {
+            "regular_season" => {
+              win_percentage: 0.5,
+              total_goals_scored: 9,
+              total_goals_against: 6,
+              average_goals_scored: 3.0,
+              average_goals_against: 2.0
+            },
+            "postseason" => {
+              win_percentage: 0.67,
+              total_goals_scored: 10,
+              total_goals_against: 7,
+              average_goals_scored: 2.5,
+              average_goals_against: 2.33
+            }
+          }
+        }
+
+        expect(@team_statistics.seasonal_summary('6')).to eq(expected_summary)
+      end
         
     end
   end
