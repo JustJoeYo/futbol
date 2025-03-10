e3dae84ee3837e49eefd0017b1754be1bbc22b34
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-10 12:02:59 -0700

    override commit error

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,287 +1,300 @@
-require 'spec_helper'
+require_relative 'statistic_helper'
 
-RSpec.configure do |config|
-  config.formatter = :documentation
-end
+class TeamStatistics
+  include StatisticHelper
 
-RSpec.describe TeamStatistics do
-  before(:each) do
-    @teams = CSV.read('./data/teams.csv', headers: true, header_converters: :symbol).map { |row| Team.new(row) }
-    @games = CSV.read('./data/games.csv', headers: true, header_converters: :symbol).map { |row| Game.new(row) }
-    @game_teams = CSV.read('./data/game_teams.csv', headers: true, header_converters: :symbol).map { |row| GameTeam.new(row) }
-
-    @team_statistics = TeamStatistics.new(@teams, @games, @game_teams)
+  def initialize(teams, games, game_teams)
+    @teams = teams
+    @games = games
+    @game_teams = game_teams
   end
 
-  describe 'initialization' do
-    it '#initialize' do
-      expect(@team_statistics).to be_an_instance_of(TeamStatistics)
-    end
-
-    it '#attributes' do
-      expect(@team_statistics.instance_variable_get(:@teams)).not_to be_empty
-      expect(@team_statistics.instance_variable_get(:@games)).not_to be_empty
-      expect(@team_statistics.instance_variable_get(:@game_teams)).not_to be_empty
+  # needs relocation to helper methods
+  # Katya
+  
+  #helper method one
+  def find_teams(team_id) 
+    @game_teams.find_all do |game|
+      team_id == game.team_id
     end
   end
 
-  describe 'instance methods' do
-    it '#team_info' do
-      expected_values = {
-        "team_id" => "18",
-        "franchise_id" => "34",
-        "team_name" => "Minnesota United FC",
-        "abbreviation" => "MIN",
-        "link" => "/api/v1/teams/18"
+  #helper method two
+  def find_games(team_id)
+    game_id_array = find_teams(team_id).map {|game| game.game_id} 
+    
+    @game_teams.find_all do |games|
+      game_id_array.include?(games.game_id) 
+    end
+  end
+
+  #helper method three
+  def group_teams(team_id) 
+    grouped_hash = find_games(team_id).group_by do |row|
+      row.team_id
+    end
+    grouped_hash.delete(team_id) 
+    grouped_hash.values #this method is returning a nested array of only the opponents rows, grouped by team_id 
+  end
+
+  #helper method four
+  def calculate_team_statistics(team_id) 
+    grouped_array = group_teams(team_id)
+    team_stats = {}
+    grouped_array.each do |team_array|
+      wins = 0
+      games = 0
+      team_array.each do |row|
+        if row.result == "WIN"
+          wins += 1
+        end
+        games += 1
+        team_stats[row.team_id] = { wins: wins, games: games }
+        end
+      end
+      team_stats 
+  end
+
+  # Katya
+  # 
+  # Andrew
+
+  def games_involving_team(team_id) #finds all games involving team
+    @game_teams.select do |game_team|
+      game_team.team_id == team_id
+    end
+  end
+
+  def find_season_by_game_id(game_id) #Finds the season for a given game_id
+    game_record = @games.find do |game|
+      game.game_id == game_id
+    end
+    game_record.season
+  end
+
+  def calculate_season_stats(games, team_id)
+    return {
+      win_percentage: 0.0,
+      total_goals_scored: 0,
+      total_goals_against: 0,
+      average_goals_scored: 0.0,
+      average_goals_against: 0.0
+    } if games.empty? #Guard against zero
+
+    total_games = games.size
+
+    total_wins = 0
+    total_goals_scored = 0
+    total_goals_against = 0
+
+    games.each do |game|
+      if game.result == "WIN"
+        total_wins += 1
+      end
+
+      total_goals_scored += game.goals.to_i
+      total_goals_against += find_opponent_score(game, team_id)
+    end
+    #Hash creation
+    {
+      win_percentage: (total_wins.to_f / total_games).round(2),
+      total_goals_scored: total_goals_scored,
+      total_goals_against: total_goals_against,
+      average_goals_scored: (total_goals_scored.to_f / total_games).round(2),
+      average_goals_against: (total_goals_against.to_f / total_games).round(2)
+    }
+  end
+
+  def find_game_type(game_id)
+    game_record = @games.find do |game|
+      game.game_id == game_id
+    end
+
+    game_record.type
+  end
+
+  def find_opponent_score(game, team_id) #Finds opponents goals from the game
+    game_record = @games.find do |g| #Using g as game is an argument
+      g.game_id == game.game_id
+    end
+
+    if game_record.home_team_id == team_id
+      game_record.away_goals.to_i
+    else
+      game_record.home_goals.to_i
+    end
+  end
+
+  def games_won_by_team(team_id) #finds all games where the team won
+    @game_teams.select do |game|
+      game.team_id == team_id && game.result == "WIN"
+    end
+  end
+
+  def games_lost_by_team(team_id) #Finds all games where the team lost
+    @game_teams.select do |game|
+      game.team_id == team_id && game.result == "LOSS"
+    end
+  end
+
+  def goal_difference(game, team_id) #Finds the goal differential
+    game_record = @games.find do |g| #Using g instead of game due to game argument
+      g.game_id == game.game_id
+    end
+
+    if game_record.home_team_id == team_id
+      (game_record.home_goals.to_i - game_record.away_goals.to_i).abs
+    elsif game_record.away_team_id == team_id
+      (game_record.away_goals.to_i - game_record.home_goals.to_i).abs
+    else
+      0 #Edge case guard
+    end
+  end
+
+  # Andrew
+  
+  # end of relocation
+
+
+  # Team Statistics
+  def team_info(team_id)
+    team = @teams.find { |t| t.team_id == team_id }
+    {
+      "team_id" => team.team_id,
+      "franchise_id" => team.franchise_id,
+      "team_name" => team.team_name,
+      "abbreviation" => team.abbreviation,
+      "link" => team.link
+    }
+  end
+
+  def best_season(team_id)
+    season_win_percentages = season_win_percentages(team_id)
+    season_win_percentages.max_by { |season, win_percentage| win_percentage }[0]
+  end
+
+  def worst_season(team_id)
+    season_win_percentages = season_win_percentages(team_id)
+    season_win_percentages.min_by { |season, win_percentage| win_percentage }[0]
+  end
+
+  def average_win_percentage(team_id)
+    total_games = @game_teams.count { |game_team| game_team.team_id == team_id }
+    return 0 if total_games.zero?
+    total_wins = @game_teams.count { |game_team| game_team.team_id == team_id && game_team.result == "WIN" }
+    (total_wins.to_f / total_games).round(2)
+  end
+
+  def most_goals_scored(team_id) #Katya
+    max_game = find_teams(team_id).max_by do |game|
+      game.goals.to_i
+    end
+    max_game.goals.to_i
+  end
+
+  def fewest_goals_scored(team_id) #Katya
+    min_game = find_teams(team_id).min_by do |game|
+      game.goals.to_i
+    end
+    min_game.goals.to_i
+  end
+
+  def favorite_opponent(team_id) #Katya
+    favorite = calculate_team_statistics(team_id).min_by do |team, stats|
+      (stats[:wins].to_f / stats[:games].to_f) * 100
+    end
+    team_name = find_team_name(favorite[0])
+    team_name
+  end
+
+  def rival(team_id) #Katya
+    rival = calculate_team_statistics(team_id).max_by do |team, stats|
+      (stats[:wins].to_f / stats[:games].to_f) * 100
+    end
+    team_name = find_team_name(rival[0])
+    team_name
+  end
+
+  def biggest_team_blowout(team_id) #Andrew
+    wins = games_won_by_team(team_id)
+
+    return 0 if wins.empty? #Edge case guard
+
+    blowouts = wins.map do |game|
+      goal_difference(game, team_id)
+    end
+
+    blowouts.max
+  end
+
+  def worst_loss(team_id) #Andrew
+    losses = games_lost_by_team(team_id)
+
+    return 0 if losses.empty? #Edge case guard
+
+    biggest_losses = losses.map do |game|
+      goal_difference(game, team_id).abs #Absolute value as the difference will be negative
+    end
+
+    biggest_losses.max
+  end
+
+  def head_to_head(team_id) #Katya
+    opponents = {}
+    opponent_stats = calculate_team_statistics(team_id) 
+    opponent_stats.each do |team, stats|
+      opponents[find_team_name(team)] = (stats[:wins].to_f / stats[:games].to_f) * 100
+    end
+    opponents
+  end
+
+  def seasonal_summary(team_id) #Andrew
+    #Step 1, get all games for the team
+    team_games = games_involving_team(team_id)
+
+    #Step 2, get unique season
+    seasons = team_games.map do |game|
+      find_season_by_game_id(game.game_id)
+    end
+
+    seasons = seasons.uniq
+
+    #Step 3, iterate through seasons and generate stats
+    summary = {}
+
+    seasons.each do |season|
+      #Find all game_ids for this season
+      season_game_ids = []
+      @games.each do |game|
+        if game.season == season
+          season_game_ids << game.game_id
+        end
+      end
+
+      #Filter game_team records based on season
+      season_games = team_games.select do |game|
+        season_game_ids.include?(game.game_id)
+      end
+
+      #separate regular/postseason games
+      regular_games = []
+      postseason_games = []
+
+      season_games.each do |game|
+        if find_game_type(game.game_id) == "Regular Season"
+          regular_games << game
+        else
+          postseason_games << game
+        end
+      end
+
+      summary[season] = {
+        "regular_season" => calculate_season_stats(regular_games, team_id),
+        "postseason" => calculate_season_stats(postseason_games, team_id)
       }
-      expect(@team_statistics.team_info('18')).to eq(expected_values)
+    
     end
 
-    it '#best_season' do 
-      expect(@team_statistics.best_season("1")).to eq("20152016")
-    end
-
-    it '#worst_season' do
-      expect(@team_statistics.worst_season("1")).to eq("20162017")
-    end
-
-    it '#average_win_percentage' do
-      expect(@team_statistics.average_win_percentage("9")).to eq(0.35)
-    end
-
-    it '#most_goals_scored' do #katya
-      expect(@team_statistics.most_goals_scored('3')).to eq(2)
-      expect(@team_statistics.most_goals_scored('14')).to eq(3)
-      expect(@team_statistics.most_goals_scored('5')).to eq(3)
-    end
-
-    it '#fewest_goals_scored' do #katya
-      expect(@team_statistics.fewest_goals_scored('3')).to eq(1)
-      expect(@team_statistics.fewest_goals_scored('14')).to eq(1)
-      expect(@team_statistics.fewest_goals_scored('5')).to eq(2)
-    end
-
-    it '#favorite_opponent' do #katya
-      expect(@team_statistics.favorite_opponent('3')).to eq("Sporting Kansas City")  
-    end
-
-    it '#rival' do #katya
-      expect(@team_statistics.rival('3')).to eq("FC Dallas") 
-    end
-
-    describe '#biggest_team_blowout' do
-      it 'returns the largest margin of victory for a given team' do
-        expect(@team_statistics.biggest_team_blowout('6')).to eq(1) #FC Dallas biggest win was 3-2 with fixture
-        expect(@team_statistics.biggest_team_blowout('3')).to eq(0) #Houston Dynamo has no wins in the fixture
-      end
-
-      it 'returns 0 if the team has no wins' do
-        expect(@team_statistics.biggest_team_blowout('9999')).to eq(0) #Fake
-      end
-    end
-
-    describe '#worst_loss' do
-      it 'returns the biggest loss for a given team' do
-        expect(@team_statistics.worst_loss('6')).to eq(0) #FC Dallas has no losses with the fixture
-        expect(@team_statistics.worst_loss('3')).to eq(1) #Houston Dynamo biggest loss was 2-3
-      end
-
-      it 'returns 0 if the team has no losses' do
-        expect(@team_statistics.worst_loss('9999')).to eq(0) #Fake
-      end
-    end
-
-    it '#head_to_head' do #katya
-       expect(@team_statistics.head_to_head('3')).to eq({"Sporting Kansas City" => 50.0, "FC Dallas" => 100.0}) 
-       expect(@team_statistics.head_to_head('6')).to eq({"Houston Dynamo"=>0.0})
-       expect(@team_statistics.head_to_head('16')).to eq({"DC United"=>100.0})
-    end
-
-    describe '#seasonal_summary' do
-      it 'returns a hash with season statistics for a given team' do
-        expected_summary = {
-          "20122013" => {
-            "regular_season" => { #No regular season games in fixture
-              win_percentage: 0.0,
-              total_goals_scored: 0,
-              total_goals_against: 0,
-              average_goals_scored: 0,
-              average_goals_against: 0
-            },
-            "postseason" => {
-              win_percentage: 1.0,
-              total_goals_scored: 11,
-              total_goals_against: 7,
-              average_goals_scored: 2.75,
-              average_goals_against: 1.75
-            }
-          }
-        }
-
-        expect(@team_statistics.seasonal_summary('6')).to eq(expected_summary)
-      end
-    end
-  end
-
-  describe 'helper methods' do
-    # Joe helpers
-
-    it '#season_win_percentages' do
-        expected_values = {
-          "20122013" => 0.36,
-          "20132014" => 0.29,
-          "20142015" => 0.39,
-          "20152016" => 0.35,
-          "20162017" => 0.41,
-          "20172018" => 0.28,
-        }
-        expect(@team_statistics.season_win_percentages("9")).to eq(expected_values)
-      end
-
-      it '#season' do
-        game_team = @game_teams.find { |gt| gt.game_id == "2012030222" }
-        expect(@team_statistics.send(:season, game_team)).to eq("20122013")
-      end
-
-    # Joe helpers
-
-
-    # katya helpers
-
-    it '#find_teams' do #katya helper method one
-      expect(@team_statistics.find_teams('14').first).to be_a(GameTeam)
-      expect(@team_statistics.find_teams('14').first.game_id).to eq('2014030413')
-      expect(@team_statistics.find_teams('14').first.team_id).to eq('14')
-      expect(@team_statistics.find_teams('14').first.giveaways).to eq('10')
-
-      expect(@team_statistics.find_teams('14')[1]).to be_a(GameTeam)
-      expect(@team_statistics.find_teams('14')[1].game_id).to eq("2014030414")
-      expect(@team_statistics.find_teams('14')[1].head_coach).to eq("Jon Cooper")
-      expect(@team_statistics.find_teams('14')[1].takeaways).to eq("7")
-    end
-
-    it "#find_games" do #katya helper method two
-      expect(@team_statistics.find_games('3').first).to be_a(GameTeam)
-      expect(@team_statistics.find_games('3').first.game_id).to eq("2012030221")
-      expect(@team_statistics.find_games('3').first.team_id).to eq("3")
-
-      expect(@team_statistics.find_games('3')[1]).to be_a(GameTeam)
-      expect(@team_statistics.find_games('3')[1].game_id).to eq("2012030221")
-      expect(@team_statistics.find_games('3')[1].team_id).to eq("6")
-      expect(@team_statistics.find_games('3')[1].result).to eq("WIN")
-
-      expect(@team_statistics.find_games('3')[7]).to be_a(GameTeam)
-      expect(@team_statistics.find_games('3')[7].team_id).to eq('5')
-      expect(@team_statistics.find_games('3')[7].tackles).to eq('35')
-    end
-
-    it "#goup_teams" do #katya helper method three
-      expect(@team_statistics.group_teams('3')).to be_a(Array)
-      expect(@team_statistics.group_teams('3')[0]).to be_a(Array)
-      expect(@team_statistics.group_teams('3')[0][0]).to be_a(GameTeam)
-      expect(@team_statistics.group_teams('3')[0][0].team_id).to eq("6")
-      expect(@team_statistics.group_teams('3')[0][0].head_coach).to eq("Claude Julien")
-      expect(@team_statistics.group_teams('3')[1][0]).to be_a(GameTeam)
-      expect(@team_statistics.group_teams('3')[1][0].team_id).to eq("5")
-      expect(@team_statistics.group_teams('3')[1][0].head_coach).to eq("Mike Sullivan")
-    end
-
-    it "#calculate_team_statistics" do #katya helper method four
-      expect(@team_statistics.calculate_team_statistics('3')).to eq({"6"=>{:wins=>3, :games=>3}, "5"=>{:wins=>1, :games=>2}})
-      expect(@team_statistics.calculate_team_statistics('16')).to eq({"14"=>{:wins=>1, :games=>1}})
-    end
-
-    # katya helpers
-
-    describe '#games_involving_team' do
-      it 'returns all game_team records for a given team' do
-        expect(@team_statistics.games_involving_team('6').size).to eq(4)
-        expect(@team_statistics.games_involving_team('3').size).to eq(5)
-        expect(@team_statistics.games_involving_team('9999')).to eq([])
-      end
-    end
-
-    describe '#find_season_by_game_id' do
-      it 'returns the correct season for a given game_id' do
-        expect(@team_statistics.find_season_by_game_id('2012030221')).to eq('20122013')
-        expect(@team_statistics.find_season_by_game_id('2015030141')).to eq('20152016')
-      end
-    end
-
-    describe '#calculate_season_stats' do
-      it 'returns correct stats for a season given game_team records' do
-        team_id = '6' #FC Dallas
-
-        games = @team_statistics.games_involving_team(team_id)
-
-        expected_stats = {
-          win_percentage: 1.0,
-          total_goals_scored: 11,
-          total_goals_against: 7,
-          average_goals_scored: 2.75,
-          average_goals_against: 1.75
-        }
-
-        expect(@team_statistics.calculate_season_stats(games, team_id)).to eq(expected_stats)
-      end
-
-      it 'returns default stats if no games are found' do
-        expected_default_stats = {
-          win_percentage: 0.0,
-          total_goals_scored: 0,
-          total_goals_against: 0,
-          average_goals_scored: 0.0,
-          average_goals_against: 0.0
-        }
-
-        expect(@team_statistics.calculate_season_stats([], '9999')).to eq(expected_default_stats)
-      end
-    end
-
-    describe '#find_game_type' do
-      it 'returns the correct type of game (Regular Season or Postseason)' do
-        expect(@team_statistics.find_game_type('2012030221')).to eq('Postseason')
-        expect(@team_statistics.find_game_type('2015030141')).to eq('Postseason')
-      end
-    end
-
-    describe '#find_opponent_score' do
-      it 'returns the number of goals scored by the opponent' do
-        team_id = '6'
-        game = @team_statistics.games_involving_team(team_id).first
-
-        expect(@team_statistics.find_opponent_score(game, team_id)).to eq(2)
-      end
-    end
-
-    describe '#games_won_by_team' do
-      it 'returns all games that a given team has won' do
-        expect(@team_statistics.games_won_by_team('6').size).to eq(4)
-        expect(@team_statistics.games_won_by_team('3').size).to eq(0)
-      end
-    end
-
-    describe '#games_lost_by_team' do
-      it 'returns all games that a give team has lost' do
-        expect(@team_statistics.games_lost_by_team('6').size).to eq(0)
-        expect(@team_statistics.games_lost_by_team('3').size).to eq(4)
-      end
-    end
-
-    describe '#goal_difference' do
-      it 'calculates the correct goal differential for a game' do
-        team_id = '6'
-        game = @team_statistics.games_involving_team(team_id).first
-
-        expect(@team_statistics.goal_difference(game, team_id)).to eq(1)
-      end
-
-      it 'returns absolute value of goal difference for losses' do
-        team_id = '3'
-        game = @team_statistics.games_involving_team(team_id).first
-
-        expect(@team_statistics.goal_difference(game, team_id)).to eq(1)
-      end
-    end
+    summary
   end
 end
